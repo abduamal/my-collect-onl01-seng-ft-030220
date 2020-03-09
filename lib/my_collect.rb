2e563588ed9e5e6239e5dfc3a8de326d2f1ec660
Amal Abdu <abduamal@ymail.com>
2020-03-09 03:30:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,13 @@
 
 
 
-def my_collect
-  
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
 
